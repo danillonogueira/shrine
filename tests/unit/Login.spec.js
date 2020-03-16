b4861817b15630895fcbecef767f0d519bf65512
Danillo <danillo.nogueira@guaranisistemas.com.br>
2020-03-16 15:32:25 -0300

    implementation of tests started

---
+++ tests/unit/Login.spec.js
@@ -0,0 +1,26 @@
+// import { shallowMount } from '@vue/test-utils'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils';
+import Login from '@/views/Login.vue';
+import Button from '@/components/Button.vue';
+
+describe('Login.vue', () => {
+  it('renders two buttons', () => {
+    const wrapper = mount(Login);
+    const buttons = wrapper.findAll(Button);
+
+    expect(buttons).toHaveLength(2);
+    wrapper.destroy();
+  });
+
+  it('has an input with type="password"', () => {
+    const wrapper = mount(Login);
+
+    expect(
+      wrapper.find('[type="password"]')
+        .exists()
+    ).toBe(true);
+
+    wrapper.destroy();
+  });
+})
